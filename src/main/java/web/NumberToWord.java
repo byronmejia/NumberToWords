ff5fb1e4c3db41cd8f70936b5675852ff9685db3
Byron Mejia <byron.mejia@connect.qut.edu.au>
2016-06-19 21:12:17 +1000

    Fixed something

--- src/main/java/web/NumberToWord.java
+++ src/main/java/web/NumberToWord.java
@@ -14,7 +14,7 @@ public class NumberToWord {
 
     NumberToWord(String word) throws NumberToWordException {
         // Number cannot be null
-        if(word == null) throw new NumberToWordException("Number cannot be null");
+        if (word == null) throw new NumberToWordException("Number cannot be null");
         // Use built in double parse library
         this.number = Double.parseDouble(word);
         // Save a copy of original string
@@ -24,93 +24,22 @@ public class NumberToWord {
     }
 
     public String toString() {
-        // If zero, just go straight to the zeroes
-        if(this.number == 0.00) {
-            this.constructionWord.add(NUMBERCONSTANTS.NUMBERS[0]);
-            return constructString("");
-        } else if(this.number < 0.00){
-            this.constructionWord.add("Negative");
-            return toString(Math.abs((int)this.number));
-        } else {
-            return toString((int)this.number);
-        }
-    }
-
-    private String toString(int temp){
-        if((temp/NUMBERCONSTANTS.PREDECIMAL_INT[3]) > 0){
-            this.constructionWord.add(
-                    extractString(
-                            (temp/NUMBERCONSTANTS.PREDECIMAL_INT[3])
-                    )
-            );
-            this.constructionWord.add(NUMBERCONSTANTS.PREDECIMAL[3]);
-            return toString(temp/NUMBERCONSTANTS.PREDECIMAL_INT[3]);
+        for (int i = 0; i < userInput.length(); i++) {
+            constructionWord.add(Character.toString(userInput.charAt(i)));
         }
 
-        if((temp/NUMBERCONSTANTS.PREDECIMAL_INT[2]) > 0){
-            this.constructionWord.add(
-                    extractString(
-                            (temp/NUMBERCONSTANTS.PREDECIMAL_INT[2])
-                    )
-            );
-            this.constructionWord.add(NUMBERCONSTANTS.PREDECIMAL[2]);
-            return toString(temp/NUMBERCONSTANTS.PREDECIMAL_INT[2]);
-        }
-
-        if((temp/NUMBERCONSTANTS.PREDECIMAL_INT[1]) > 0){
-            this.constructionWord.add(
-                    extractString(
-                            (temp/NUMBERCONSTANTS.PREDECIMAL_INT[1])
-                    )
-            );
-            this.constructionWord.add(NUMBERCONSTANTS.PREDECIMAL[1]);
-            return toString(temp/NUMBERCONSTANTS.PREDECIMAL_INT[1]);
-        }
-
-        if((temp/NUMBERCONSTANTS.PREDECIMAL_INT[0]) > 0){
-            this.constructionWord.add(
-                    extractString(
-                            (temp/NUMBERCONSTANTS.PREDECIMAL_INT[0])
-                    )
-            );
-            this.constructionWord.add(NUMBERCONSTANTS.PREDECIMAL[0]);
-            return toString(temp/NUMBERCONSTANTS.PREDECIMAL_INT[0]);
-        }
-
-        this.constructionWord.add(
-                extractString(
-                        (temp)
-                )
-        );
+        if (constructionWord.contains(".")) constructionWord.set(constructionWord.indexOf("."), "and");
 
         return constructString("");
     }
 
-    private String extractString(int temp){
-        if(temp < 10){
-            return NUMBERCONSTANTS.NUMBERS[(int)temp] + " ";
-        } else if(temp < 20) {
-            return NUMBERCONSTANTS.TEENS[temp - 10] + " ";
-        } else {
-            String string = NUMBERCONSTANTS.TENS[temp/10];
-            int tempUnit = temp % 10;
-            if((temp % 10) > 0) {
-                return string + "-" + NUMBERCONSTANTS.NUMBERS[tempUnit] + " ";
-            } else {
-                return string + " ";
-            }
-        }
-    }
-
     private String constructString(String solution) {
-        System.out.println(this.constructionWord.toString());
         String tempString = this.constructionWord.get(0);
         this.constructionWord.remove(0);
-        System.out.println(solution + tempString);
-        if(constructionWord.isEmpty()){
+        if (constructionWord.isEmpty()){
             return solution + tempString;
         } else {
-            return constructString(solution + tempString);
+            return constructString(solution + tempString + " ");
         }
     }
 }
